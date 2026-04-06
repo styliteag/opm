7679af0c9e37b6e9bd3c7e242dbc8bc547ace53e
Wim Bonis <wb@stylite.de>
2026-04-06 14:09:14 +0200

    feat: add allowed hosts configuration for Vite server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -58,6 +58,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: [".trycloudflare.com"],
     proxy: {
       "/api": {
         target: process.env.DOCKER
